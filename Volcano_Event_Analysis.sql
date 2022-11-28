3ef72c959003d0a57bdb2569a6630428e9a5c2b5
maldinf <118784145+maldinf@users.noreply.github.com>
2022-11-28 17:30:21 +0700

    Update Volcano_Event_Analysis.sql

--- Volcano_Event_Analysis.sql
+++ Volcano_Event_Analysis.sql
@@ -21,53 +21,59 @@ drop column total_houses_destroyed,
 drop column total_houses_destroyed_description
 
 1. Provinsi mana yang paling banyak memiliki letusan gunungapi 
-
-## Melihat total erupsi di setiap pulau di Indonesia
+## Membagi setiap gunung terhadap provinsinya masing masing
 select 
-	island,
-	count(erupt_date) as total_erupt
-from volcanoevent v 
-group by island
-order by total_erupt desc
+	case
+		when name_volcano = 'Sinabung' then 'Sumatra Utara'
+		when name_volcano = 'Kraktau' then 'Lampung'
+		when name_volcano in ('Cereme', 'Papandayan', 'Galunggung', 'Tangkuban Parahu', 'Salak') then 'Jawa Barat'
+		when name_volcano in ('Dieng Volcanic Complex', 'Merapi') then 'Jawa Tengah'
+		when name_volcano in ('Ijen', 'Semeru', 'Raung', 'Kelut', 'Lamongan', 'Semeru', 'Tengger Caldera') then 'Jawa Timur'
+		when name_volcano in ('Awu', 'Ruang', 'Banua Wuhu', 'Karangetang', 'Lokon-Empung', 'Soputan', 'Mahawu') then 'Sulawesi Utara'
+		when name_volcano = 'Colo' then 'Sulawesi Tengah'
+		when name_volcano in ('Dukono', 'Gamkonora', 'Gamalama', 'Makian') then 'Maluku Utara'
+		when name_volcano = 'Wurlali' then 'Banda'
+		when name_volcano in ('Tambora', 'Rinjani') then 'Nusa Tenggara Barat'
+		when name_volcano in ('Agung', 'Batur')  then 'Bali'
+		else 'Nusa Tenggara Timur'
+	end as provinsi_pernama_gunung,
+	count(erupt_date) as total_eruptdate
+from volcanoevent v
+group by provinsi_pernama_gunung
+order by total_eruptdate desc
 
-## Melihat Gunung yang aktif di Pulau Jawa ##
+## Melihat sejarah erupsi gunungapi yang berada di Jawa Timur##
 select 
 	distinct name_volcano
 from volcanoevent v 
 where island = 'Java'
 
 ## Melihat gunungapi yang memiliki letusan terbanyak di Pulau Jawa ##
-with volcanE as
+with vulcanEr as
 	(select 
-		year,
 		name_volcano,
-		island,
-		count(erupt_date) as total_erupt,
-		vei
-	from volcanoevent v 
-	where island = 'Java'
-	)
-select 
-	island,
-	total_erupt
-from volcanE
-group by island
-order by vei desc
+		erupt_date,
+		case
+			when name_volcano = 'Sinabung' then 'Sumatra Utara'
+			when name_volcano = 'Kraktau' then 'Lampung'
+			when name_volcano in ('Cereme', 'Papandayan', 'Galunggung', 'Tangkuban Parahu', 'Salak') then 'Jawa Barat'
+			when name_volcano in ('Dieng Volcanic Complex', 'Merapi') then 'Jawa Tengah'
+			when name_volcano in ('Ijen', 'Semeru', 'Raung', 'Kelut', 'Lamongan', 'Semeru', 'Tengger Caldera') then 'Jawa Timur'
+			when name_volcano in ('Awu', 'Ruang', 'Banua Wuhu', 'Karangetang', 'Lokon-Empung', 'Soputan', 'Mahawu') then 'Sulawesi Utara'
+			when name_volcano = 'Colo' then 'Sulawesi Tengah'
+			when name_volcano in ('Dukono', 'Gamkonora', 'Gamalama', 'Makian') then 'Maluku Utara'
+			when name_volcano = 'Wurlali' then 'Banda'
+			when name_volcano in ('Tambora', 'Rinjani') then 'Nusa Tenggara Barat'
+			when name_volcano in ('Agung', 'Batur')  then 'Bali'
+			else 'Nusa Tenggara Timur'
+		end as provinsi_pernama_gunung
+	from volcanoevent v )
+select *
+from vulcanEr
+where provinsi_pernama_gunung = 'Jawa Timur'
+order by erupt_date 
 
 2. Gunungapi apa saja yang letusannya yang diikuti oleh tsunami dan gempabumi  
-select 
-	year,
-	location,
-	name_volcano,
-	count(tsunami) as total_tsunami,
-	count(gempabumi) as total_gempabumi
-from volcanoevent v 
-where 
-	tsunami is not null or 
-	gempabumi is not null
-group by name_volcano 
-order by total_tsunami desc
-
 with vulcan as 
 	(select 
 		year,
@@ -104,8 +110,8 @@ with volcan as
 		location,
 		name_volcano,
 		count(erupt_date) as total_erupt,
-		count(deaths) as total_mati,
-		count(injuries) as total_injuries 
+		sum(deaths) as total_mati,
+		sum(injuries) as total_injuries 
 	from volcanoevent v 
 	group by name_volcano 
 	order by total_mati desc
@@ -116,5 +122,6 @@ select
 	total_mati,
 	total_injuries
 from volcan
+limit 10
 
 
